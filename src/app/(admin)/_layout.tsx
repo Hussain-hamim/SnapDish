2ea9cb31883c2389f9072412c67db3958674d892
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-29 21:54:03 +0430

    improve bottom tab nav and menu items

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -1,30 +1,53 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-
+import { Redirect, Tabs } from 'expo-router';
+import { useColorScheme } from 'react-native';
 import Colors from '../../constants/Colors';
-import { StatusBar } from 'expo-status-bar';
+import { useAuth } from '@/providers/AuthProvider';
 
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
+function TabBarIcon({
+  name,
+  color,
+  focused,
+}: {
   name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
+  focused: boolean;
 }) {
-  return <FontAwesome size={20} style={{ marginBottom: -3 }} {...props} />;
+  return (
+    <FontAwesome
+      name={name}
+      size={focused ? 26 : 22}
+      color={color}
+      style={{ marginBottom: -2 }}
+    />
+  );
 }
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const { session } = useAuth();
+
+  if (!session) {
+    return <Redirect href='/' />;
+  }
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors.light.background,
-        tabBarInactiveTintColor: 'gainsboro',
+        headerShown: false,
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+        },
         tabBarStyle: {
-          backgroundColor: Colors.light.tint,
+          backgroundColor: '#fff',
+          borderTopWidth: 0,
+          height: 70,
+          paddingBottom: 10,
+          paddingTop: 10,
+          elevation: 10,
         },
       }}
     >
@@ -34,18 +57,19 @@ export default function TabLayout() {
         name='menu'
         options={{
           title: 'Menu',
-          headerShown: false,
-          tabBarIcon: ({ color }) => (
-            <TabBarIcon name='cutlery' color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name='cutlery' color={color} focused={focused} />
           ),
         }}
       />
+
       <Tabs.Screen
         name='orders'
         options={{
           title: 'Orders',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name='list' color={color} />,
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name='list' color={color} focused={focused} />
+          ),
         }}
       />
     </Tabs>
