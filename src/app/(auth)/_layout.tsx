93db262ccb9b7f1bd9a7db02308963abbac1aaa5
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-29 18:13:04 +0430

    navigation

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,5 +1,12 @@
-import { Stack } from 'expo-router';
+import { useAuth } from '@/providers/AuthProvider';
+import { Redirect, Stack } from 'expo-router';
 
 export default function AuthLayout() {
+  const { session } = useAuth();
+
+  if (session) {
+    <Redirect href={'/'} />;
+  }
+
   return <Stack />;
 }
