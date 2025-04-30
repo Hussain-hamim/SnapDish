8e481b168776cb605e3e6da7e13ffb350477fdf7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-30 16:07:52 +0430

    delete product

--- src/app/(admin)/menu/create.tsx
+++ src/app/(admin)/menu/create.tsx
@@ -5,7 +5,12 @@ import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, TextInput, Image, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
-import { useInsertProduct, useProduct, useUpdateProduct } from '@/api/products';
+import {
+  useDeleteProduct,
+  useInsertProduct,
+  useProduct,
+  useUpdateProduct,
+} from '@/api/products';
 
 const CreateProductScreen = () => {
   const [name, setName] = useState('');
@@ -21,6 +26,7 @@ const CreateProductScreen = () => {
   const { mutate: insertProduct, error } = useInsertProduct();
   const { mutate: updateProduct } = useUpdateProduct();
   const { data: updatingProduct } = useProduct(myId);
+  const { mutate: deleteProduct } = useDeleteProduct();
 
   useEffect(() => {
     if (updatingProduct) {
@@ -106,8 +112,14 @@ const CreateProductScreen = () => {
       setImage(result.assets[0].uri);
     }
   };
+
   const onDelete = () => {
-    console.warn('DELETE!!!!!!!');
+    deleteProduct(myId, {
+      onSuccess: () => {
+        resetFields();
+        router.replace('/(admin)');
+      },
+    });
   };
 
   const confirmDelete = () => {
