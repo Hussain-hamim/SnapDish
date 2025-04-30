efdd49003a8db038e2df2a57d3a86891c2219c97
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-04-30 12:26:44 +0430

    fetch data for admin

--- src/app/(admin)/menu/index.tsx
+++ src/app/(admin)/menu/index.tsx
@@ -1,9 +1,18 @@
-import { FlatList } from 'react-native';
-// import products from '@assets/data/products';
-import products from '../../../../assets/data/products';
+import { ActivityIndicator, FlatList } from 'react-native';
 import ProductListItem from '@/components/ProductListItem';
+import { useProductList } from '@/api/products';
+import { Text } from 'react-native';
 
 export default function MenuScreen() {
+  const { error, data: products, isLoading } = useProductList();
+
+  if (isLoading) {
+    return <ActivityIndicator size='large' color={'orchid'} />;
+  }
+  if (error) {
+    return <Text>Failed fetching products</Text>;
+  }
+
   return (
     <FlatList
       data={products}
