0dfc50e41f854c3c60e536bd341a0077cb952cb1
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-05-07 23:45:47 +0430

    feat: enhance NotificationProvider with notification handling and logging

--- src/providers/NotificationProvider.tsx
+++ src/providers/NotificationProvider.tsx
@@ -5,14 +5,54 @@ import * as Notifications from 'expo-notifications';
 import { supabase } from '@/lib/supabase';
 import { useAuth } from './AuthProvider';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
 const NotificationProvider = ({ children }: PropsWithChildren) => {
   const [expoPushToken, setExpoPushToken] = useState<String | undefined>();
+  const [notification, setNotification] =
+    useState<Notifications.Notification>();
+
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  const { profile } = useAuth();
+  console.log(expoPushToken);
 
   useEffect(() => {
     registerForPushNotificationsAsync()
       .then((token) => setExpoPushToken(token ?? ''))
       .catch((error: any) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      if (notificationListener.current) {
+        Notifications.removeNotificationSubscription(
+          notificationListener.current
+        );
+      }
+      if (responseListener.current) {
+        Notifications.removeNotificationSubscription(responseListener.current);
+      }
+    };
   }, []);
+
+  console.log('Push token: ', expoPushToken);
+  console.log('Notif: ', notification);
 
   return <>{children}</>;
 };
